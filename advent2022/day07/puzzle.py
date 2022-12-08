b4c2be1812bf914fec19d7ccf4a74ea4011a157e
gareth <gaz@grufnet.com>
2022-12-08 18:45:51 +0000

    advent2022 day07

---
+++ advent2022/day07/puzzle.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code
+"""
+import os
+import re
+
+COMMAND_CD = re.compile(r'\$ cd (.*)')
+COMMAND_LS = re.compile(r'\$ ls')
+LISTING_DIR = re.compile(r'dir (.*)')
+LISTING_FILE = re.compile(r'(\d+) (.*)')
+
+
+class Folder():
+
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.child_folder = {}
+        self.child_file = {}
+
+    def __repr__(self):
+        return os.path.join(str(self.parent), self.name)
+
+    def add_child_folder(self, name):
+        if name not in self.child_folder.keys():
+            child = Folder(name, self)
+            self.child_folder[name] = child
+        else:
+            child = self.child_folder[name]
+        return child
+
+    def add_child_file(self, name, size):
+        if name not in self.child_file.keys():
+            self.child_file[name] = int(size)
+        else:
+            exit("duplicate filename?")
+        return name
+
+    def size(self):
+        size = 0
+        for n, f in self.child_folder.items():
+            s = f.size()
+            size += s
+            # print(self.name, f, size, s)
+        for name, s in self.child_file.items():
+            size += s
+            # print(self.name, name, size, s)
+        return size
+
+
+def solve(terminal_output):
+
+    # process terminal output
+    folder_list = []
+    folder_root = Folder('#ROOT#', None)
+    folder_current = folder_root
+
+    for line in terminal_output:
+        if match := COMMAND_CD.search(line):
+            name = match.group(1)
+            if name == '..':
+                folder_current = folder_current.parent
+            else:
+                folder_current = folder_current.add_child_folder(name)
+                folder_list.append(folder_current)
+        elif COMMAND_LS.search(line):
+            # print('do nothing - ', line, folder_current)
+            continue
+        else:
+            if match := LISTING_FILE.search(line):
+                name = match.group(2)
+                size = match.group(1)
+                folder_current.add_child_file(name, size)
+            elif match := LISTING_DIR.search(line):
+                # print('do nothing - ', line, match.group(1), folder_current)
+                continue
+
+    # part 01
+    part01_folder_sum = sum([f.size()
+                            for f in folder_list if f.size() <= 100000])
+
+    # part 02
+    file_system_size = 70000000
+    space_update = 30000000
+    folder_root_size = folder_root.size()
+
+    space_unused = file_system_size - folder_root_size
+    space_required = space_update - space_unused
+
+    folder_list_space = [(f, f.size())
+                         for f in folder_list if f.size() >= space_required]
+    folder_list_sorted = sorted(folder_list_space, key=lambda x: x[1])
+    shortest_folder = folder_list_sorted.pop(0)
+    part02_folder_size = shortest_folder[1]
+
+    # return results
+    return (part01_folder_sum, part02_folder_size)
+
+
+def input_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    # read input data from file
+    with open(input_data_file, 'r') as input_filehandle:
+        input_data_text_list = input_filehandle.read().splitlines()
+
+    return input_data_text_list
+
+
+if __name__ == '__main__':
+    input_data = input_data('input.txt')
+
+    answer = solve(input_data)
+    print(f"Total sizes of those directories less than 100000 is {answer[0]}")
+
+    answer = solve(input_data)
+    print(f"Size of smallest directory to free space = {answer[1]}")
---
+++ advent2022/day07/test_puzzle.py
@@ -0,0 +1,35 @@
+"""
+Advent of Code - Test Case
+"""
+import unittest
+
+import advent2022.day07.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+
+    example_answer_part01 = 95437
+    example_answer_part02 = 24933642
+
+    answer_part01 = 1513699
+    answer_part02 = 7991939
+
+    def test_input_example(self):
+        input_data = puzzle.input_data('input_example.txt')
+
+        answer = puzzle.solve(input_data)
+        answer_part01 = answer[0]
+        self.assertEqual(answer_part01, self.example_answer_part01)
+
+        answer_part02 = answer[1]
+        self.assertEqual(answer_part02, self.example_answer_part02)
+
+    def test_input(self):
+        input_data = puzzle.input_data('input.txt')
+
+        answer = puzzle.solve(input_data)
+        answer_part01 = answer[0]
+        self.assertEqual(answer_part01, self.answer_part01)
+
+        answer_part02 = answer[1]
+        self.assertEqual(answer_part02, self.answer_part02)
