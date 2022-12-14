f68a911a087692d29f3125c8e3d6abe8b0b3930b
gareth <gaz@grufnet.com>
2022-12-14 15:10:14 +0000

    advent2022 day12

--- advent2022/day12/puzzle.py
+++ advent2022/day12/puzzle.py
@@ -8,16 +8,17 @@ import matplotlib as mpl
 import os
 import numpy as np
 
-# visualisation
+# Visualisations
 # make a color map of fixed colors
 COLOR_MAP1 = mpl.colors.LinearSegmentedColormap.from_list('height_gradient_map',
                                                           ['green', 'blue',
                                                               'white'],
                                                           256)
 COLOR_MAP2 = mpl.colors.LinearSegmentedColormap.from_list('height_gradient_map',
-                                                          ['red', 'orange',
-                                                              'yellow'],
+                                                          ['white', 'black'],
                                                           256)
+
+DEAD_END = 999
 
 
 class GridMap():
@@ -28,64 +29,83 @@ class GridMap():
         self.bounds_n, self.bounds_w = (0, 0)
         self.bounds_s, self.bounds_e = self.height_map.shape
 
+        self.start = self.find_start_loc()
+        self.end = self.find_end_loc()
+
+    def find_start_loc(self):
         # find start location (r,c)
         s = np.where(self.height_map == np.min(self.height_map))
         sl = list(zip(s[0], s[1]))
-        self.start = sl[0]
+        start = sl[0]
+        return start
 
+    def find_end_loc(self):
         # find end location (r,c)
         e = np.where(self.height_map == np.max(self.height_map))
-        se = list(zip(e[0], e[1]))
-        self.end = se[0]
+        el = list(zip(e[0], e[1]))
+        end = el[0]
+        return end
 
-        # create and initialise
+    def distance_map_init(self, loc_s):
+        # create and initialise distance map
         self.distance_map = np.zeros(self.height_map.shape, dtype=int,)
-        # number bigger than possible number of steps
-        self.distance_map[:] = ((self.bounds_s + 1) * (self.bounds_e + 1)) ^ 2
-        self.distance_map[self.start] = 0
+        self.distance_map[:] = DEAD_END
+        self.distance_map[loc_s] = 0
 
-    def next_steps(self, loc_c):
+    def next_moves(self, loc_c):
+        # find all valid next move (N,E,S,W)
         r, c = loc_c
-        steps = []
+        moves = []
         # check N
         if r > self.bounds_n:
             loc_n = (r - 1, c)
             if self.valid_move(loc_c, loc_n):
-                steps.append(loc_n)
+                moves.append(loc_n)
         # check E
         if (c + 1) < self.bounds_e:
             loc_n = (r, c + 1)
             if self.valid_move(loc_c, loc_n):
-                steps.append(loc_n)
+                moves.append(loc_n)
         # check S
         if (r + 1) < self.bounds_s:
             loc_n = (r + 1, c)
             if self.valid_move(loc_c, loc_n):
-                steps.append(loc_n)
+                moves.append(loc_n)
         # check W
         if c > self.bounds_w:
             loc_n = (r, c - 1)
             if self.valid_move(loc_c, loc_n):
-                steps.append(loc_n)
-        return steps
+                moves.append(loc_n)
+        return moves
 
+    # Valid move is up one step (or down as many as required)
+    # Note: this may result in path jumping down into hole
+    #       from end cannot be reached.
     def valid_move(self, loc_c, loc_n):
         h_diff = self.height_map[loc_n] - self.height_map[loc_c]
         if h_diff <= 1:
             return True
         return False
 
-    def calc_distance_to_end(self):
+    # calculate shortest distance from start to end
+    # Note: not completely accurate?
+    def calc_distance_to_end(self, loc_s=None, loc_e=None):
+        if not loc_s:
+            loc_s = self.find_start_loc()
+
+        if not loc_e:
+            loc_e = self.find_end_loc()
+
+        self.distance_map_init(loc_s)
 
         q = []  # queue of locations & distances
-        q.append((self.start, 0))  # initial with start location
+        q.append((loc_s, 0))  # initial with start location
 
         # walk map to find shortest distance
         while len(q) > 0:
             loc_c, distance_c = q.pop()
-            if self.distance_map[loc_c] != distance_c:
-                continue
-            for loc_n in self.next_steps(loc_c):
+
+            for loc_n in self.next_moves(loc_c):
                 distance_n = distance_c + 1
                 if (self.distance_map[loc_n] > distance_n):
                     self.distance_map[loc_n] = distance_n
@@ -129,12 +149,56 @@ def solve(height_map_text):
     grid_map = GridMap([list(map(convert_height, i))
                         for i in height_map_text.split("\n")])
 
-    shortest_distance = grid_map.calc_distance_to_end()
+    # part 01
 
+    part01 = grid_map.calc_distance_to_end()
+    # grid_map.visualise()
+
+    # part 02
+
+    # Brute force starting place
+    # need to revisit calc_distance_to_end
+    hm = grid_map.height_map
+
+    # produce map with levels 1 & 2
+    level = 1
+    lm = np.where(hm <= (level + 1), hm, 0)
+
+    # Create filter for dead-ends
+    for ir, ic in np.ndindex(lm.shape):
+        if lm[ir, ic] != level:
+            continue
+        if (ir > 0) and (lm[ir - 1, ic] == (level + 1)):
+            continue
+        if (ic > 0) and (lm[ir, ic - 1] == (level + 1)):
+            continue
+        if (ir < (lm.shape[0] - 1)) and (lm[ir + 1, ic] == (level + 1)):
+            continue
+        if (ic < (lm.shape[1] - 1)) and (lm[ir, ic + 1] == (level + 1)):
+            continue
+        lm[ir, ic] = 0
+    lm = np.where(lm == level, lm, 0)
+
+    # All possible start places
+    possible_s = list(zip(*np.where(lm == 1)))
+
+    # Find shortest distance to end for all possible stating places
+    loc_s = None
+    distance_s = DEAD_END
+    for loc_n in possible_s:
+        distance_n = grid_map.calc_distance_to_end(loc_n)
+        if distance_n < distance_s:
+            distance_s = distance_n
+            loc_s = loc_n
+        # print(f"distance starting at {loc_n} took {distance_n}")
+
+    # print(f"Shortest distance starts at {loc_s} took {distance_s}")
+
+    part02 = distance_s
     # grid_map.visualise()
 
     # return results
-    return shortest_distance
+    return (part01, part02)
 
 
 def input_data(filename):
@@ -148,7 +212,8 @@ def input_data(filename):
 
 
 if __name__ == '__main__':
-    input_data = input_data('input_example.txt')
+    input_data = input_data('input.txt')
 
     answer = solve(input_data)
-    print(f"path reaches the goal in {answer} steps.\n")
+    print(f"part01 - Path reaches the goal in {answer[0]} move.\n")
+    print(f"part02 - Shortest path from elevation 'a' is {answer[1]} move.\n")
--- advent2022/day12/test_puzzle.py
+++ advent2022/day12/test_puzzle.py
@@ -9,25 +9,23 @@ import advent2022.day12.puzzle as puzzle
 class PuzzleTest(unittest.TestCase):
 
     example_answer_part01 = 31
-    example_answer_part02 = 0
+    example_answer_part02 = 29
 
     answer_part01 = 456
-    answer_part02 = 0
+    answer_part02 = 454
 
     def test_input_example(self):
         input_data = puzzle.input_data('input_example.txt')
 
-        answer_part01 = puzzle.solve(input_data)
-        self.assertEqual(answer_part01, self.example_answer_part01)
+        answer = puzzle.solve(input_data)
+        self.assertEqual(answer[0], self.example_answer_part01)
 
-        # answer_part02 = puzzle.solve(input_data, 10000, True)
-        # self.assertEqual(answer_part02, self.example_answer_part02)
+        self.assertEqual(answer[1], self.example_answer_part02)
 
     def test_input(self):
         input_data = puzzle.input_data('input.txt')
 
-        answer_part01 = puzzle.solve(input_data)
-        self.assertEqual(answer_part01, self.answer_part01)
+        answer = puzzle.solve(input_data)
+        self.assertEqual(answer[0], self.answer_part01)
 
-        #answer_part02 = puzzle.solve(input_data, 10000, True)
-        #self.assertEqual(answer_part02, self.answer_part02)
+        self.assertEqual(answer[1], self.answer_part02)
