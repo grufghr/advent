8929dcc5f6c5a5345f7e951921150b576008b50b
gareth <gaz@grufnet.com>
2022-12-21 16:32:06 +0000

    advent2022 day19

---
+++ advent2022/day19/test_puzzle.py
@@ -0,0 +1,29 @@
+"""
+Advent of Code - Test Case
+"""
+import unittest
+
+import advent2022.day19.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+
+    example_answer_part01 = 33
+    example_answer_part02 = 2604
+
+    answer_part01 = 790
+    answer_part02 = 7350
+
+    def test_input_example(self):
+        input_data = puzzle.input_data('input_example.txt')
+
+        answer = puzzle.solve(input_data)
+        self.assertEqual(answer[0], self.example_answer_part01)
+        self.assertEqual(answer[1], self.example_answer_part02)
+
+    def test_input(self):
+        input_data = puzzle.input_data('input.txt')
+
+        answer = puzzle.solve(input_data)
+        self.assertEqual(answer[0], self.answer_part01)
+        self.assertEqual(answer[1], self.answer_part02)
