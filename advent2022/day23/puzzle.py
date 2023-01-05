d38f342dce488a1286be7c191aad6be1604e1e44
gareth <gaz@grufnet.com>
2023-01-05 23:30:01 +0000

    performance advent2022 day23
    replaced list with set

--- advent2022/day23/puzzle.py
+++ advent2022/day23/puzzle.py
@@ -6,7 +6,6 @@ Advent of Code
 import os
 import operator
 
-
 dirs = {"N": [(0, -1), (1, -1), (-1, -1)],
         "S": [(0, 1), (1, 1,), (-1, 1)],
         "W": [(-1, 0), (-1, 1), (-1, -1)],
@@ -18,14 +17,14 @@ def add(t1, t2):
     return tuple(map(operator.add, t1, t2))
 
 
-def visualise(grove_map):
-    x_vals = [x for x, y in grove_map]
-    y_vals = [y for x, y in grove_map]
+def visualise_grove(elves_list):
+    x_vals = [x for x, y in elves_list]
+    y_vals = [y for x, y in elves_list]
 
     for y in range(min(y_vals) - 1, max(y_vals) + 1):
         line = ''
         for x in range(min(x_vals) - 1, max(x_vals) + 1):
-            if (x, y) in grove_map:
+            if (x, y) in elves_list:
                 line += '#'
             else:
                 line += '.'
@@ -37,32 +36,37 @@ def solve(grove_map_list, max_rounds):
     direction_order = ["N", "S", "W", "E"]
 
     # parse input
-    grove_map = []
+    elves_list = []
     for y, line in enumerate(grove_map_list):
-        for x, pos in enumerate(line):
-            if pos == '#':
+        for x, grid in enumerate(line):
+            if grid == '#':
                 elf = (x, y)
-                grove_map.append(elf)
+                elves_list.append(elf)
 
-    # visualise(grove_map)
+    # visualise_grove(elves_list)
 
     # part 01 - move elves for 10 rounds
-
+    # part 02 - round when no elves move
     round_num = 0
     movement = True
     while round_num < max_rounds and movement:
         round_num += 1
-        print("round ", round_num)
+        # print("round ", round_num)
+
+        # note: "x in y" is faster with sets than list
+        # with set takes 0.317 secs
+        # with list takes 11.082 secs
+        elves_set = set(elves_list)
 
         # 1st part of round
         moves = {}
-        for idx, elf in enumerate(grove_map):
-            if not any(add(elf, direction) in grove_map for direction in direction_all):
+        for idx, elf in enumerate(elves_list):
+            if not any(add(elf, direction) in elves_set for direction in direction_all):
                 # elf does need to move
                 continue
 
             for diro in direction_order:
-                if not any(add(elf, direction) in grove_map for direction in dirs[diro]):
+                if not any(add(elf, direction) in elves_set for direction in dirs[diro]):
                     elf_p = add(elf, dirs[diro][0])
                     if elf_p not in moves.keys():
                         moves[elf_p] = idx
@@ -74,21 +78,21 @@ def solve(grove_map_list, max_rounds):
         # rotate direction order queue
         direction_order.append(direction_order.pop(0))
 
-        # 2nd part of round
+        # 2nd part of round - move elves if move unique
         movement = False
         for elf_p, idx in moves.items():
             if idx is not None:
-                grove_map[idx] = elf_p
+                elves_list[idx] = elf_p
                 movement = True
 
-        # visualise(grove_map)
+        # visualise_grove(elves_list)
 
-    x_vals = [x for x, y in grove_map]
-    y_vals = [y for x, y in grove_map]
+    x_vals = [x for x, y in elves_list]
+    y_vals = [y for x, y in elves_list]
     grid_size = (max(x_vals) - min(x_vals) + 1) * \
         (max(y_vals) - min(y_vals) + 1)
 
-    empty_grids = grid_size - len(grove_map)
+    empty_grids = grid_size - len(elves_list)
 
     return (empty_grids, round_num)
 
