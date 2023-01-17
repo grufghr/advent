d41a128c767857673212333694b2404e8781ffe9
gareth <gaz@grufnet.com>
2023-01-17 19:33:31 +0000

    advent2016 day01

---
+++ advent2016/day01/puzzle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2016
+"""
+import os
+import re
+
+
+INSTRUCTION_REGEX = re.compile(r"(L|R)(\d+)")
+
+
+def solve(instruction_text):
+
+    # part 01 - start at 0,0 facing North
+    pos = (0, 0)
+    compass = ['N', 'E', 'S', 'W']
+    #compass = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+    # part 02
+    visited = set()
+    visited.add(pos)
+    bunny_hq = None
+
+    instruction_list = [x.strip() for x in instruction_text.split(',')]
+    for instruction in instruction_list:
+        match_i = INSTRUCTION_REGEX.search(instruction)
+        direction, steps = match_i.groups()
+        steps = int(steps)
+
+        if direction == 'L':
+            compass = compass[1:] + list(compass[0])
+        elif direction == 'R':
+            compass = list(compass[-1]) + compass[:-1]
+
+        for s in range(0, steps):
+
+            if compass[0] == 'N':
+                pos = (pos[0], pos[1] - 1)
+            elif compass[0] == 'E':
+                pos = (pos[0] + 1, pos[1])
+            elif compass[0] == 'S':
+                pos = (pos[0], pos[1] + 1)
+            elif compass[0] == 'W':
+                pos = (pos[0] - 1, pos[1])
+
+            if not bunny_hq:
+                if pos not in visited:
+                    visited.add(pos)
+                else:
+                    bunny_hq = pos
+
+    distance01 = sum([abs(x) for x in list(pos)])
+
+    if bunny_hq:
+        distance02 = sum([abs(x) for x in list(bunny_hq)])
+    else:
+        distance02 = None
+
+    # return results
+    return (distance01, distance02)
+
+
+def input_data_iter(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as input_filehandle:
+        input_txt_list = input_filehandle.read().splitlines()
+
+    for input_txt in input_txt_list:
+        yield (input_txt)
+
+
+if __name__ == '__main__':
+    instruction_set = next(input_data_iter('input.txt'))
+
+    answer = solve(instruction_set)
+
+    print(f"Easter Bunny HQ = {answer[0]} blocks")
+    print(f"first location you visit twice = {answer[1]} blocks")
---
+++ advent2016/day01/test_puzzle.py
@@ -0,0 +1,35 @@
+"""
+Advent of Code - Test Case
+"""
+import unittest
+
+import advent2016.day01.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+
+    example_answer_part01 = [5, 2, 12, 8]
+    example_answer_part02 = [None, None, None, 4]
+
+    answer_part01 = 242
+    answer_part02 = 150
+
+    def test_input_example(self):
+        for test_case, input_text in enumerate(puzzle.input_data_iter('input_example.txt')):
+
+            answer = puzzle.solve(input_text)
+            answer01 = answer[0]
+            self.assertEqual(answer01, self.example_answer_part01[test_case])
+
+            answer02 = answer[1]
+            self.assertEqual(answer02, self.example_answer_part02[test_case])
+
+    def test_input(self):
+        input_text = next(puzzle.input_data_iter('input.txt'))
+
+        answer = puzzle.solve(input_text)
+        answer01 = answer[0]
+        self.assertEqual(answer01, self.answer_part01)
+
+        answer02 = answer[1]
+        self.assertEqual(answer02, self.answer_part02)
