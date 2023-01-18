e5cea0b32920deffb8c66047ceb201fdb16ad7c2
gareth <gaz@grufnet.com>
2023-01-18 16:34:57 +0000

    refactor
    remove redundant init files
    add performance test cases (2015)

---
+++ advent2015/day06/test_performance.py
@@ -0,0 +1,26 @@
+"""
+Advent of Code - Performance test
+"""
+import unittest
+import time
+
+import advent2015.day06.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+
+    time_part01 = 1.0
+    time_part02 = 1.0
+
+    def test_performance(self):
+        input_data = puzzle.input_data('input.txt')
+
+        ts = time.time()
+        puzzle.solve(input_data)
+        t = time.time() - ts
+        self.assertLess(t, self.time_part01, f"part01 {t:2.5f} secs")
+
+        ts = time.time()
+        puzzle.solve(input_data)
+        t = time.time() - ts
+        self.assertLess(t, self.time_part02, f"part02 {t:2.5f} secs")
--- advent2022/day21/test_puzzle.py
+++ advent2022/day21/test_puzzle.py
@@ -24,14 +24,14 @@ class PuzzleTest(unittest.TestCase):
         self.assertEqual(answer[0], self.example1_answer_part01)
         self.assertEqual(answer[1], self.example1_answer_part02)
 
-    def test_001_input_example2(self):
+    def test_002_input_example2(self):
         input_data = puzzle.input_data('input_example2.txt')
 
         answer = puzzle.solve(input_data)
         self.assertEqual(answer[0], self.example2_answer_part01)
         self.assertEqual(answer[1], self.example2_answer_part02)
 
-    def test_002_input(self):
+    def test_003_input(self):
         input_data = puzzle.input_data('input.txt')
 
         answer = puzzle.solve(input_data)
