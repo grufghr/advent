be375ec8d900fae762d0d0b05bb6099ffc97eeb7
gareth <gaz@grufnet.com>
2023-02-01 01:10:09 +0000

    advent2020 day02

---
+++ advent2020/day02/puzzle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code - Solve Puzzle 2020 Day 02: Password Philosophy
+"""
+import os
+import re
+
+
+POLICY_REGEX = re.compile(r"(\d+)-(\d+) ([a-z]): ([a-z]*)")
+
+
+def solve01(input_data):
+    # part 01 - count valid lines
+    valid_tot = 0
+    for line in input_data:
+        regex_match = POLICY_REGEX.search(line)
+        min_count, max_count, letter, pwd = regex_match.groups()
+
+        c = pwd.count(letter)
+        if c >= int(min_count) and c <= int(max_count):
+            valid_tot += 1
+
+    return valid_tot
+
+
+def solve02(input_data):
+    # part 02 - count valid lines
+    valid_tot = 0
+    for line in input_data:
+        regex_match = POLICY_REGEX.search(line)
+        pos_a, pos_b, letter, pwd = regex_match.groups()
+
+        a = (pwd[int(pos_a) - 1] == letter)
+        b = (pwd[int(pos_b) - 1] == letter)
+        if (a and not b) or (not a and b):
+            valid_tot += 1
+
+    return valid_tot
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as input_filehandle:
+        input_data_text_list = input_filehandle.readlines()
+
+    return input_data_text_list
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f"part01 - valid lines = {answer01}")
+
+    answer02 = solve02(input_data)
+    print(f"part02 - valid lines = {answer02}")
