6c0f671c6ee47b6e0929db7f8cb4e3768f78b375
gareth <gaz@grufnet.com>
2023-11-21 21:01:00 +0000

    advent2017 day04

---
+++ advent2017/day04/test_puzzle.py
@@ -0,0 +1,58 @@
+"""
+Advent of Code - Test
+"""
+import unittest
+import time
+
+import advent2017.day04.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+    example_data_01 = [
+        ("aa bb cc dd ee", True),
+        ("aa bb cc dd aa", False),
+        ("aa bb cc dd aaa", True),
+    ]
+    example_data_02 = [
+        ("abcde fghij", True),
+        ("abcde xyz ecdab", False),
+        ("a ab abc abd abf abj", True),
+        ("iiii oiii ooii oooi oooo", True),
+        ("oiii ioii iioi iiio", False),
+    ]
+
+    answer01 = 325
+    answer02 = 119
+
+    execution_time = 1.0
+
+    def test_001_examples_01(self):
+        for input_data, expected_result in self.example_data_01:
+            with self.subTest(input_data):
+                result = puzzle.policy01(input_data)
+                self.assertEqual(result, expected_result)
+
+    def test_002_puzzle_01(self):
+        input_data = puzzle.load_data("input.txt")
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.answer01)
+
+    def test_003_examples_02(self):
+        for input_data, expected_result in self.example_data_02:
+            with self.subTest(input_data):
+                result = puzzle.policy02(input_data)
+                self.assertEqual(result, expected_result)
+
+    def test_004_puzzle_02(self):
+        input_data = puzzle.load_data("input.txt")
+        answer02 = puzzle.solve02(input_data)
+        self.assertEqual(answer02, self.answer02)
+
+    def test_005_performance(self):
+        input_data = puzzle.load_data("input.txt")
+        ts = time.time()
+        answer02 = puzzle.solve02(input_data)
+        t = time.time() - ts
+        self.assertEqual(answer02, self.answer02)
+        self.assertLess(t, self.execution_time, f"part02 {t:2.5f} secs")
+        print(f"execution_time {t:2.5f} secs")
