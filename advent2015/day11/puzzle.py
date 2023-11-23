c444187ecc4088123ca7036b803dd22ede98cd59
gareth <gaz@grufnet.com>
2023-11-23 20:41:40 +0000

    advent2015 day11

---
+++ advent2015/day11/puzzle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2015 Day 11: Corporate Policy
+"""
+import os
+from itertools import groupby
+from string import ascii_lowercase
+
+
+def solve01(input_data):
+    password = next_password(input_data)
+    while not is_valid(password):
+        password = next_password(password)
+    return password
+
+
+def solve02(input_data):
+    return solve01(input_data)
+
+
+def is_valid(password):
+    # fmt: off
+    # cannot contain i, o, l
+    if len(set('iol') & set(password)) > 0:
+        return False
+    
+    # must contain 3 sequential i.e. abc
+    if not any([ascii_lowercase[n: n + 3] in password for n in range(24)]):
+        return False
+
+    # contains two non-overlapping pairs
+    if sum([2 if len(list(group)) >= 4 else 1 for letter, group in groupby(password) if len(list(group)) >= 2]) < 2:
+        return False
+
+    # fmt: on
+    return True
+
+
+def next_password(password):
+    if password == "":
+        return ""
+    elif password[-1] < "z":
+        return password[0:-1] + chr(ord(password[-1]) + 1)
+    else:
+        return next_password(password[:-1]) + "a"
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    # read input data from file
+    with open(input_data_file, "r") as filehandle:
+        input_data = filehandle.read()
+
+    return input_data
+
+
+if __name__ == "__main__":
+    input_data = load_data("input.txt")
+
+    answer01 = solve01(input_data)
+    print(f"part01 - santas next password = {answer01}")
+
+    # note: pass part01 answer to part02
+    answer02 = solve01(answer01)
+    print(f"part02 - santas next password = {answer02}")
---
+++ advent2015/day11/test_puzzle.py
@@ -0,0 +1,57 @@
+"""
+Advent of Code - Test
+"""
+import unittest
+import time
+
+import advent2015.day11.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+    # fmt: off
+    example_data_01 = [
+        ("abcdefgh", "abcdffaa"),
+        ("ghijklmn", "ghjaabcc")
+    ]
+    testdata_000 = [
+        ("hijklmmn", False),
+        ("abbceffg", False),
+        ("abbcegjk", False),
+        ("abcdffaa", True),
+        ("ghjaabcc", True),
+        ("cqjxxyzz", True),
+        ("cqkaabcc", True),
+    ]
+    # fmt: on
+
+    answer01 = "cqjxxyzz"
+    answer02 = "cqkaabcc"
+
+    execution_time = 4.0
+
+    def test_000_is_valid(self):
+        for input_data, expected_result in self.testdata_000:
+            with self.subTest(input_data):
+                result = puzzle.is_valid(input_data)
+                self.assertEqual(result, expected_result)
+
+    def test_001_example_01(self):
+        for input_data, expected_result in self.example_data_01:
+            with self.subTest(input_data):
+                result = puzzle.solve01(input_data)
+                self.assertEqual(result, expected_result)
+
+    def test_002_solve_01(self):
+        input_data = puzzle.load_data("input.txt")
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.answer01)
+
+    def test_004_solve_02(self):
+        # note: answer01 used as input to part02
+        input_data = self.answer01
+        ts = time.time()
+        answer02 = puzzle.solve02(input_data)
+        t = time.time() - ts
+        self.assertEqual(answer02, self.answer02)
+        self.assertLess(t, self.execution_time, f"part02 {t:2.5f} secs")
+        print(f"execution_time {t:2.5f} secs")
