8611bae28075815cba93b9fb367fbdda0f28a994
gareth <gaz@grufnet.com>
2023-12-01 17:04:13 +0000

    advent2015 day10

---
+++ advent2015/day10/puzzle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2015 Day 10: Elves Look, Elves Say
+"""
+import os
+import re
+
+LS_REGEX = re.compile(r'(1+|2+|3+|4+|5+|6+|7+|8+|9+|0+)')
+
+
+def solve01(input_data):
+    number = input_data
+    for step in range(0, 40):
+        number_n = ''
+        for match in LS_REGEX.findall(number):
+            seq = str(len(match)) + match[0]
+            number_n = number_n + seq
+        number = number_n
+    return len(number)
+
+
+def solve02(input_data):
+    number = input_data
+    for step in range(0, 50):
+        number_n = ''
+        for match in LS_REGEX.findall(number):
+            seq = str(len(match)) + match[0]
+            number_n = number_n + seq
+        number = number_n
+    return len(number)
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    return input_data
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - look-and-say after 40 iteratons = {answer01}')
+
+    answer02 = solve02(input_data)
+    print(f'part02 - look-and-say after 50 iteratons = {answer02}')
---
+++ advent2015/day10/test_puzzle.py
@@ -0,0 +1,41 @@
+"""
+Advent of Code - Test
+"""
+import unittest
+import time
+
+import advent2015.day10.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+    example_answer01 = 82350
+    example_answer02 = 1166642
+
+    answer01 = 360154
+    answer02 = 5103798
+
+    execution_time = 7.0
+
+    def test_001_example_01(self):
+        input_data = puzzle.load_data('input_example.txt')
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.example_answer01)
+
+    def test_002_solve_01(self):
+        input_data = puzzle.load_data('input.txt')
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.answer01)
+
+    def test_003_example_02(self):
+        input_data = puzzle.load_data('input_example.txt')
+        answer02 = puzzle.solve02(input_data)
+        self.assertEqual(answer02, self.example_answer02)
+
+    def test_004_solve_02(self):
+        input_data = puzzle.load_data('input.txt')
+        ts = time.time()
+        answer02 = puzzle.solve02(input_data)
+        t = time.time() - ts
+        self.assertEqual(answer02, self.answer02)
+        self.assertLess(t, self.execution_time, f'part02 {t:2.5f} secs')
+        print(f'execution_time {t:2.5f} secs')
