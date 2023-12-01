5f3ba0e68453f663c9072552ac417ee87875d854
gareth <gaz@grufnet.com>
2023-12-01 17:09:42 +0000

    advent2021 day25

---
+++ advent2021/day25/puzzle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2021 Day 25: Sea Cucumber
+"""
+import os
+import numpy as np
+
+
+def solve01(cucumber_grid):
+    # create new empty grid
+    r_size, c_size = cucumber_grid.shape
+
+    movement = True
+    step = 0
+
+    while movement and (step < 1000):
+        movement = False
+
+        cucumber_grid_n = cucumber_grid.copy()
+
+        # east heard
+        for r in range(r_size):
+            for c in range(c_size):
+                if cucumber_grid[r][c] == '>':
+                    c_n = (c + 1) % c_size
+                    if cucumber_grid[r][c_n] == '.':
+                        cucumber_grid_n[r][c] = '.'
+                        cucumber_grid_n[r][c_n] = '>'
+                        movement = True
+                    else:
+                        cucumber_grid_n[r][c] = '>'
+
+        cucumber_grid = cucumber_grid_n.copy()
+
+        # south heard
+        for r in range(r_size):
+            for c in range(c_size):
+                if cucumber_grid[r][c] == 'v':
+                    r_n = (r + 1) % r_size
+                    if cucumber_grid[r_n][c] == '.':
+                        cucumber_grid_n[r][c] = '.'
+                        cucumber_grid_n[r_n][c] = 'v'
+                        movement = True
+                    else:
+                        cucumber_grid_n[r][c] = 'v'
+
+        cucumber_grid = cucumber_grid_n.copy()
+        step += 1
+
+    return step
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    # parse data
+    # split data into 2d array
+    cucumber_map_text = [list(i) for i in input_data.splitlines()]
+    # convert into numpy array
+    cucumber_grid = np.array(cucumber_map_text)
+
+    return cucumber_grid
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - First step on which no sea cucumbers moved = {answer01}')
---
+++ advent2021/day25/test_puzzle.py
@@ -0,0 +1,31 @@
+"""
+Advent of Code - Test
+"""
+import unittest
+import time
+
+import advent2021.day25.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+    example_answer01 = 58
+    example_answer02 = None
+
+    answer01 = 532
+    answer02 = None
+
+    execution_time = 20.0
+
+    def test_001_example_01(self):
+        input_data = puzzle.load_data('input_example.txt')
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.example_answer01)
+
+    def test_002_solve_01(self):
+        input_data = puzzle.load_data('input.txt')
+        ts = time.time()
+        answer01 = puzzle.solve01(input_data)
+        t = time.time() - ts
+        self.assertEqual(answer01, self.answer01)
+        self.assertLess(t, self.execution_time, f'part02 {t:2.5f} secs')
+        print(f'execution_time {t:2.5f} secs')
