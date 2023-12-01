3aac388a0045f5fb61de3602cbfacbe5c3ce375b
gareth <gaz@grufnet.com>
2023-12-01 16:51:16 +0000

    advent2015 day03

---
+++ advent2015/day03/puzzle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2015 Day 03: Perfectly Spherical Houses in a Vacuum
+"""
+import os
+import numpy as np
+
+
+def solve01(input_data):
+    return solve(input_data)
+
+
+def solve02(input_data):
+    return solve(input_data, 2)
+
+
+def solve(instruction_set, delivery_num=1):
+    # create basic 3x3 array (to start)
+    present_np = np.zeros((3, 3), dtype=int)
+
+    # all delivery people start at centre of present_np [r,c]
+    pos = np.array([[1, 1]] * delivery_num)
+
+    # each delivery person delivers present to first house
+    present_np[1][1] += delivery_num
+
+    # each delivery person moves according to instruction
+    for i, instruction in enumerate(instruction_set):
+        p = i % delivery_num
+        if instruction == 'v':
+            pos[p][0] += 1
+        elif instruction == '^':
+            pos[p][0] -= 1
+        elif instruction == '>':
+            pos[p][1] += 1
+        elif instruction == '<':
+            pos[p][1] -= 1
+        else:
+            exit(f'Unknown instructions {instruction}')
+
+        # insert/append to present_np if delivery person is out of bounds
+        shape = present_np.shape
+        if pos[p][0] > (shape[0] - 1):
+            zr = np.zeros((1, shape[1]), dtype=int)
+            present_np = np.append(present_np, zr, axis=0)
+        elif pos[p][0] < 0:
+            present_np = np.insert(present_np, 0, 0, axis=0)
+            # adjust all delivery people (to account for insert)
+            pos[:, 0] += 1
+            pos[p][0] = 0
+        elif pos[p][1] > (shape[1] - 1):
+            zc = np.zeros((shape[0], 1), dtype=int)
+            present_np = np.append(present_np, zc, axis=1)
+        elif pos[p][1] < 0:
+            present_np = np.insert(present_np, 0, 0, axis=1)
+            # adjust all delivery people (to account for insert)
+            pos[:, 1] += 1
+            pos[p][1] = 0
+
+        # deliver present
+        present_np[pos[p][0]][pos[p][1]] += 1
+
+    houses_visited_count = np.count_nonzero(present_np)
+
+    return houses_visited_count
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    return input_data
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - Houses visited with santa = {answer01}')
+
+    answer02 = solve02(input_data)
+    print(f'part02 - Houses visited with santa & robot = {answer02}')
---
+++ advent2015/day03/test_puzzle.py
@@ -0,0 +1,47 @@
+"""
+Advent of Code - Test
+"""
+import unittest
+import time
+
+import advent2015.day03.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+    example_answer01 = [2, 4, 2, 2]
+    example_answer02 = [2, 3, 11, 3]
+
+    answer01 = 2081
+    answer02 = 2341
+
+    execution_time = 1.0
+
+    def _input_data_iter(self, input_data):
+        for tc, input_data_tc in enumerate(input_data.splitlines()):
+            yield tc, input_data_tc
+
+    def test_001_example_01(self):
+        input_data = puzzle.load_data('input_example.txt')
+        for tc, input_data_tc in self._input_data_iter(input_data):
+            answer01 = puzzle.solve01(input_data_tc)
+            self.assertEqual(answer01, self.example_answer01[tc])
+
+    def test_002_solve_01(self):
+        input_data = puzzle.load_data('input.txt')
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.answer01)
+
+    def test_003_example_02(self):
+        input_data = puzzle.load_data('input_example.txt')
+        for tc, input_data_tc in self._input_data_iter(input_data):
+            answer02 = puzzle.solve02(input_data_tc)
+            self.assertEqual(answer02, self.example_answer02[tc])
+
+    def test_004_solve_02(self):
+        input_data = puzzle.load_data('input.txt')
+        ts = time.time()
+        answer02 = puzzle.solve02(input_data)
+        t = time.time() - ts
+        self.assertEqual(answer02, self.answer02)
+        self.assertLess(t, self.execution_time, f'part02 {t:2.5f} secs')
+        print(f'execution_time {t:2.5f} secs')
