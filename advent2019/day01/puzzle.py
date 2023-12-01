859798de7a4fcbe45bf0f453ef6c03938d0d6bc0
gareth <gaz@grufnet.com>
2023-12-01 17:09:06 +0000

    advent2019 day01

---
+++ advent2019/day01/puzzle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2019 Day 01: The Tyranny of the Rocket Equation
+"""
+import os
+
+
+def solve01(input_data):
+    # part 01 - calculate fuel required for all modules
+    fuel_tot = 0
+    for mass in input_data:
+        fuel = (mass // 3) - 2
+        fuel_tot += fuel
+    return fuel_tot
+
+
+def solve02(input_data):
+    # part 02 - calculate fuel required for all modules (including fuel)
+    fuel_tot = 0
+    for mass_m in input_data:
+        # fuel required for module
+        fuel_m = (mass_m // 3) - 2
+        fuel_tot += fuel_m
+
+        # fuel required for fuel
+        mass_f = fuel_m
+        while mass_f > 0:
+            mass_f = (mass_f // 3) - 2
+            if mass_f > 0:
+                fuel_tot += mass_f
+
+    return fuel_tot
+
+
+def parse_data(input_data):
+    input_data = input_data.splitlines()
+    return [int(x) for x in input_data]
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    return parse_data(input_data)
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - fuel needs = {answer01}')
+
+    answer02 = solve02(input_data)
+    print(f'part02 - fuel needs (including fuel needs) = {answer02}')
