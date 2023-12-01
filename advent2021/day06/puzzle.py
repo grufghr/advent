2c6cee59d70e19b90cc31c96fedf92ad80bc7009
gareth <gaz@grufnet.com>
2023-12-01 17:09:42 +0000

    advent2021 day06

---
+++ advent2021/day06/puzzle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2021 Day 06: Lanternfish
+"""
+import os
+
+
+def solve01(input_data):
+    return solve(input_data, 80)
+
+
+def solve02(input_data):
+    return solve(input_data, 256)
+
+
+def solve(fish_age_list, days):
+    # part 01&02 - fish population after n days
+    day = 0
+    population_data = [fish_age_list.count(age) for age in range(0, 9)]
+
+    while day < days:
+        day += 1
+
+        population_data.append(population_data[0])
+        fish_6 = population_data.pop(0)
+        population_data[6] += fish_6
+
+    # print(f"After  {day} day: population = {sum(population_data)}")
+    fish_pop = sum(population_data)
+
+    return fish_pop
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    # parse data
+    fish_text_list = list(input_data.split(','))
+    fish_age_list = list(map(int, fish_text_list))
+    return fish_age_list
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - fish population after 80 days = {answer01}')
+
+    answer02 = solve02(input_data)
+    print(f'part02 - fish population after 256 days = {answer02}')
