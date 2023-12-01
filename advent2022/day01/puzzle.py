37579931a8315f86b23b33485f10a678e33a5105
gareth <gaz@grufnet.com>
2023-12-01 16:44:06 +0000

    advent2022 day01

---
+++ advent2022/day01/puzzle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2022 Day 01: Calorie Counting
+"""
+import os
+
+
+def solve01(input_data):
+    # part 01
+
+    # create array of totals of each elf inventory
+    inventory_totals = list(map(sum, input_data))
+
+    # find largest elf inventory totals
+    inventory_max = max(inventory_totals)
+    return inventory_max
+
+
+def solve02(input_data):
+    # part 02
+
+    # create array of totals of each elf inventory
+    inventory_totals = list(map(sum, input_data))
+
+    # sort inventory totals (largest first)
+    inventory_totals_sorted = list(inventory_totals)
+    inventory_totals_sorted.sort(reverse=True)
+
+    # split top 3 into seperate list
+    inventory_totals_top3 = inventory_totals_sorted[:3]
+
+    # sum top 3 totals
+    inventory_totals_top3_sum = sum(inventory_totals_top3)
+
+    return inventory_totals_top3_sum
+
+
+def parse_data(input_data):
+    # split data into list of lists (ints)
+    inventory_text = [i.splitlines() for i in input_data.split('\n\n')]
+    inventory = [list(map(int, i)) for i in inventory_text]
+
+    return inventory
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    return parse_data(input_data)
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - Total Calories strongest Elf = {answer01}')
+
+    answer02 = solve02(input_data)
+    print(f'part02 - Total Calories top three Elves = {answer02}')
