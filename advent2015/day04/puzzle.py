caae2945480ad999b04534cebc3a911a0561c033
gareth <gaz@grufnet.com>
2023-12-01 17:01:20 +0000

    advent2015 day04

---
+++ advent2015/day04/puzzle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2015 Day 04: The Ideal Stocking Stuffer
+"""
+import os
+import hashlib
+
+
+def solve01(input_data):
+    answer = solve(input_data, 5)
+    return answer
+
+
+def solve02(input_data):
+    answer = solve(input_data, 6)
+    return answer
+
+
+def solve(input_text, n):
+    lowest_positive_number = 0
+    n_zeroes = '0' * n
+
+    found = False
+    while (not found) and (lowest_positive_number < 99999999):
+        lowest_positive_number += 1
+
+        text = input_text + str(lowest_positive_number)
+
+        hash_text = str(hashlib.md5(text.encode('utf-8')).hexdigest())
+        if hash_text.startswith(n_zeroes):
+            found = True
+
+    return lowest_positive_number
+
+
+def load_data(filename):
+    input_data_file = os.path.join(os.path.dirname(__file__), filename)
+
+    with open(input_data_file, 'r') as filehandle:
+        input_data = filehandle.read()
+
+    return input_data
+
+
+if __name__ == '__main__':
+    input_data = load_data('input.txt')
+
+    answer01 = solve01(input_data)
+    print(f'part01 - lowest_positive_number = {answer01}')
+
+    answer02 = solve02(input_data)
+    print(f'part02 - lowest_positive_number = {answer02}')
---
+++ advent2015/day04/test_puzzle.py
@@ -0,0 +1,47 @@
+"""
+Advent of Code - Test
+"""
+import unittest
+import time
+
+import advent2015.day04.puzzle as puzzle
+
+
+class PuzzleTest(unittest.TestCase):
+    example_answer01 = [609043, 1048970]
+    example_answer02 = [6742839, 5714438]
+
+    answer01 = 254575
+    answer02 = 1038736
+
+    execution_time = 2.0
+
+    def _input_data_iter(self, input_data):
+        for tc, input_data_tc in enumerate(input_data.splitlines()):
+            yield tc, input_data_tc
+
+    def test_001_example_01(self):
+        input_data = puzzle.load_data('input_example.txt')
+        for tc, input_data_tc in self._input_data_iter(input_data):
+            answer01 = puzzle.solve01(input_data_tc)
+            self.assertEqual(answer01, self.example_answer01[tc])
+
+    def test_002_solve_01(self):
+        input_data = puzzle.load_data('input.txt')
+        answer01 = puzzle.solve01(input_data)
+        self.assertEqual(answer01, self.answer01)
+
+    def test_003_example_02(self):
+        input_data = puzzle.load_data('input_example.txt')
+        for tc, input_data_tc in self._input_data_iter(input_data):
+            answer02 = puzzle.solve02(input_data_tc)
+            self.assertEqual(answer02, self.example_answer02[tc])
+
+    def test_004_solve_02(self):
+        input_data = puzzle.load_data('input.txt')
+        ts = time.time()
+        answer02 = puzzle.solve02(input_data)
+        t = time.time() - ts
+        self.assertEqual(answer02, self.answer02)
+        self.assertLess(t, self.execution_time, f'part02 {t:2.5f} secs')
+        print(f'execution_time {t:2.5f} secs')
