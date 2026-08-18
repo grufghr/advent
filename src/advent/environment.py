700a05c2db0b01499f9bdc1c58d17125342ac097
gareth <gaz@grufnet.com>
2026-08-18 02:19:11 +0100

    update
    update
      - behave environment location

--- src/advent/environment.py
+++ src/advent/environment.py
@@ -4,8 +4,10 @@ Feature Test Environment functions
 
 # general imports
 import csv
+import os
 import logging
 
+# logging config
 logging.basicConfig(
     filename='./stats/stats.log',
     filemode='a',
@@ -13,12 +15,17 @@ logging.basicConfig(
     datefmt='%H:%M:%S',
     level=logging.DEBUG,
 )
+# suppress parse WARNING
+logging.getLogger('parse').setLevel(logging.WARNING)
 
+# create logger
 logger = logging.getLogger(__name__)
 
-stats_file = './stats/stats.csv'
+# stats file
+stats_file = os.path.abspath('./stats/stats.csv')
 
 
+# after scenario
 def after_scenario(context, scenario):
     if context.input_file and context.input_file == 'input.txt':
         data = [context.year, context.day, context.funcname, context.timer, context.feature.name]
